550c3f4393fa491834c5377db161dcab58189db8
Royce Sibichan <92983553+Royce-Sibi@users.noreply.github.com>
2022-05-30 10:40:27 +0100

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAPQAVFpP3lAoGv7GPCnZO6K6t6RXQefRQ",
+    authDomain: "project-94-f19f5.firebaseapp.com",
+    projectId: "project-94-f19f5",
+    storageBucket: "project-94-f19f5.appspot.com",
+    messagingSenderId: "79833894860",
+    appId: "1:79833894860:web:48024a4b1b96efd7789769"
+};
+    firebase.initializeApp(firebaseConfig);
